4c360f77e1f449e871fb5f2c07e3e02341c93f35
david-3lm <d.3lopm@gmail.com>
2024-05-12 12:11:53 +0200

    Game!

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+	Start: 'Welcome to my Portfolio, stay close to the objects around the path to learn more about me!',
+	CV: 'Here! You can check my CV with all my data and professional information! <a href="public/PDF/CV_DAVIDLOPEZ.pdf" target="_blank"> Get PDF! </a>',
+	RRSS: 'Check out my Socials here: <br> <a href="https://github.com/david-3lm" target="_blank">Github</a> <a href="https://www.linkedin.com/in/david-l%C3%B3pez-l%C3%B3pez-95b2a5228/" target="_blank">LinkedIn</a>',
+	Projects: 'This is the Path of Projects, where I show you the projects I have worked on',
+	Chess: 'Chess: Holy War, is a videogame about Chess, with additions of RPG. <br> In this project I was the main programmer and Scrum master, I made from scratch a whole AI to play chess and all the mechanichs inside the game! <br> <a href="https://chubbycat-games.itch.io/chess-holywar" target="_blank">Play it here!</a>',
+	ITHCIO: 'You can see all my projects uploaded in Itch.io <a href="https://david-3lm.itch.io/" target="_blank"> here.</a> Some of them my participations in Game Jams.',
+	Cyberhell: 'I also participated in android game projects, my best one: Cyberhell, a fast-paced bullet hell. It is uploaded at the play store and you can download it <a href="https://play.google.com/store/apps/details?id=com.ChubbyCat.com.unity.template.mobile2D" target="_blank">here.</a>',
+	Telefonica: 'At the moment, I am expanding my frontiers by studying at 42 Madrid by Fundacion Telefonica. It is a place where I learn about programming in a Peer-2-Peer enviroment. <br> My knowledge doesn\'t stop in the videogames.',
+	Interests: 'From here, you enter in the Path of Interests, here I show my main branches of Interests.',
+	Investigation: 'Invastigating is a main part in my life, and doing it in tech fills my heart. In my last project (which was my Bachelor\'s Thesis) I made a projector integration for Unity to make interactive experiences at museums and schools.',
+	Videogames: 'Of course one of my passions is the videogames, and I love being part of the production and development of a game. I\'m looking forward to develop more and more experiences.',
+	VRAI: 'A part from that, I am also really interested in Virtual Reality and Artificial Inteligence and its development, I also made some cool projects in this fields.',
+	Studies: 'This is the last one, The Path of Studies!',
+	URJC: 'I studied  <b>Video Game Desing and Develompent</b> at Rey Juan Carlos University in Madrid, here, I learned most of my knowledge about programming and videogame development. <b> But not everyithing I will learn! </b>',
+	Courses: 'I love taking courses that improve my programming skills and make me more valuable. At the moment, I am learning about Unreal Engine 5!',
+	JAM: 'I also learned a lot about working as a team and deveolping by participating in some Game Jams, where you have to develop a videogame with a deathline about a specific theme. It\'s cool, and some of my games are published in my itch.io ready to play!!',
+	Campfire: 'And you have arrived to the end of my journey... For the moment, the best part is yet to come. <br> Just relax for a bit near the campfire, and after that you can check my CV for more information! Thank you for your time!! <br> <a href="public/PDF/CV_DAVIDLOPEZ.pdf" target="_blank"> Get PDF! </a>',
+  };
---
+++ src/main.js
@@ -0,0 +1,126 @@
+import { dialogueData, scaleFactor } from "./constants";
+import { k } from "./kaboomCtx";
+import { displayDialogue, setCamScale } from "./utils";
+
+k.loadSprite("spritesheet", "./colored_packed.png",{
+	sliceX: 49,
+	sliceY: 22,
+	anims: {
+		"idle-down" : 361,
+		"walk-down" : { from: 362, to: 364, loop: true, speed:8},
+		"idle-side" : 361,
+		"walk-side" : { from: 362, to: 364, loop: true, speed:8},
+		"idle-up" : 361,
+		"walk-up" : { from: 362, to: 364, loop: true, speed:8}
+	}
+
+});
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#462d3d"));
+
+k.scene("main", async () => {
+	const mapData = await(await fetch("./map.json")).json();
+	const layers = mapData.layers;
+
+	const map = k.add([
+		k.sprite("map"),
+		k.pos(0),
+		k.scale(scaleFactor)
+	]);
+
+	const player = k.make([
+		k.sprite("spritesheet", {anim: "idle-down"}),
+		k.area({
+			shape: new k.Rect(k.vec2(0,3), 10, 10),
+		}),
+		k.body(),
+		k.anchor("center"),
+		k.pos(),
+		k.scale(scaleFactor),
+		{
+			speed: 250,
+			direction: "down",
+			isInDialogue: false,
+		},
+		"player",
+	]);
+
+	for (const layer of layers)
+	{
+		if (layer.name === "boundaries")
+		{
+			for (const boundary of layer.objects)
+			{
+				map.add([
+					k.area({
+						shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+					}),
+					k.body({isStatic: true}),
+					k.pos(boundary.x, boundary.y),
+					boundary.name,
+				]);
+
+				if (boundary.name)
+				{
+					player.onCollide(boundary.name, () => {
+						player.isInDialogue = true;
+						displayDialogue(dialogueData[boundary.name], () => (player.isInDialogue = false));
+					})
+				}
+			}
+			continue;
+		}
+
+		if (layer.name === "spawnpoint")
+		{
+			for (const entity of layer.objects)
+			{
+				if (entity.name === "player")
+				{
+					player.pos = k.vec2(
+						(map.pos.x + entity.x) * scaleFactor,
+						(map.pos.y + entity.y) * scaleFactor
+					);
+
+					k.add(player);
+					continue;
+				}
+			}
+		}
+	}
+
+	setCamScale(k);
+
+	k.onResize(() => {
+		setCamScale(k);
+	});
+
+	k.onUpdate(() => {
+		k.camPos(player.pos.x, player.pos.y + 50);
+	})
+
+	k.onMouseDown((mouseBtn) => {
+		if (mouseBtn !== "left" || player.isInDialogue) return;
+
+		const worldMousePos = k.toWorld(k.mousePos());
+		player.moveTo(worldMousePos, player.speed);
+
+		if (worldMousePos.x < player.pos.x)
+			player.flipX = true;
+		else
+			player.flipX = false;
+		if (player.curAnim() !== "walk-down")
+		{
+
+			player.play("walk-down");
+		}
+	});
+
+	k.onMouseRelease(() => {
+		player.play("idle-down");
+	})
+});
+
+k.go("main");
