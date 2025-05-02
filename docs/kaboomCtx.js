3e34503ea7eba6451974d898bdd60ad67d42acd5
david-3lm <d.3lopm@gmail.com>
2025-05-02 13:12:34 +0200

    Cambios cambios

--- docs/kaboomCtx.js
+++
@@ -1,9 +0,0 @@
-import kaboom from "https://unpkg.com/kaboom@3000/dist/kaboom.mjs";
-
-const k = kaboom({
-    global: false,
-    touchToMouse: true,
-    canvas: document.getElementById("game"),
-});
-
-export default k;
